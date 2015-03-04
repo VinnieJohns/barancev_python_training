ac2e1a0ec389cc519cf64af69e4b0d45b590988f
VinnieJohns <novikov.d12@gmail.com>
2015-03-05 03:08:54 +0400

    Task #3

---
+++ contact.py
@@ -0,0 +1,26 @@
+__author__ = 'VinnieJohns'
+
+
+class Contact:
+    def __init__(self, fname, midname, lname, nickname, title, company, address, home_tel, mobile_tel, work_tel,
+                 fax, email_2, email_3, homepage, birth_date, anniversary_date, secondary_address,
+                 secondary_home_phone, notes):
+        self.fname = fname
+        self.midname = midname
+        self.lname = lname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_tel = home_tel
+        self.mobile_tel = mobile_tel
+        self.work_tel = work_tel
+        self.fax = fax
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.birth_date = birth_date
+        self.anniversary_date = anniversary_date
+        self.secondary_address = secondary_address
+        self.secondary_home_phone = secondary_home_phone
+        self.notes = notes
