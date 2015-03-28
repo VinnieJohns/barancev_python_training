00853702618cf77ebcf4cec25f5db46b6b4d6f67
VinnieJohns <novikov.d12@gmail.com>
2015-03-28 16:05:56 +0400

    Task #8
    fixture validation

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,5 +5,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="ololo", header="olalala", footer="olololo"))
 
+
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
